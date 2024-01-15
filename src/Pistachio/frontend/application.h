ca7f3d8b7e22bf793cf541b27a7d6b48ca04ac60
ThomasGons <gons.thomas@gmail.com>
2024-01-16 00:07:46 +0100

    add: general layout, freetype for better font rendering and Noto font family

---
+++ src/Pistachio/frontend/application.h
@@ -0,0 +1,36 @@
+//
+// Created by thomas on 15/01/24.
+//
+#pragma once
+
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+
+
+class Application {
+    SDL_Window* window;
+    SDL_GLContext gl_context;
+    ImGuiIO* io;
+
+public:
+    Application();
+    ~Application();
+    void run();
+
+private:
+        /**
+     * @brief generic error logger
+     * @tparam Args variadic template for the arguments
+     * @param fmt the format string
+     * @param args the arguments
+     */
+    template<typename... Args>
+    void logError(const char *fmt, Args... args);
+};
+
+
